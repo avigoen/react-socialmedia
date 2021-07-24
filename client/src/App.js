c339c09c5d25f6af72b666ddc7453fe44460d608
Avinash Goen <agoen97@gmail.com>
2021-07-24 19:26:02 +0530

    Get All Posts on Homepage

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'semantic-ui-react';
 
+import './App.css'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+import { Grid } from 'semantic-ui-react'
+import PostCard from '../component/PostCard'
 
 function Home() {
+    const { loading, data, error } = useQuery(FETCH_POSTS_QUERY)
+    if (loading) {
+        return (<h1>Loading...</h1>)
+    }
+    const { getPosts: posts } = data
     return (
-        <div><h1>Home</h1></div>
+        <Grid columns={3}>
+            <Grid.Row className="page-title">
+                <h1>Recently Posts</h1>
+            </Grid.Row>
+            <Grid.Row>
+                {posts && posts.map(post => (
+                    <Grid.Column key={post.id}>
+                        <PostCard post={post} />
+                    </Grid.Column>
+                ))}
+            </Grid.Row>
+        </Grid>
     )
 }
+
+const FETCH_POSTS_QUERY = gql`
+{
+    getPosts{
+        id
+        body
+        createdAt
+        username
+        likeCount
+        likes {
+            username
+        }
+        commentCount
+        comments {
+            id username createdAt body
+        }
+    }
+}
+`
 
 export default Home
